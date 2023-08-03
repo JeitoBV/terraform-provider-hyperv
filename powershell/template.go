361c90e37bf7b574c8def1d3514b50e7642f2748
Frank Spijkerman <frank@jeito.nl>
2023-08-03 14:24:41 +0200

    using start-process

--- powershell/template.go
+++ powershell/template.go
@@ -51,7 +51,7 @@ var elevatedCommandTemplate = template.Must(template.New("ElevatedCommand").Func
   $username = '{{escapeSingleQuotes .User}}'.Replace('\.\\', $env:computername+'\');
   $password = '{{escapeSingleQuotes .Password}}';
   $credential = New-Object System.Management.Automation.PsCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))
-  powershell -NoProfile -Credential $credential -NoNewWindow -ExecutionPolicy Bypass -File "{{escapeSingleQuotes .ScriptPath}}"
+  Start-Process -Credential $credential -Wait -NoNewWindow -FilePath "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File {{.ScriptPath}}"
 `))
 
 // var elevatedCommandTemplate = template.Must(template.New("ElevatedCommand").Funcs(template.FuncMap{
